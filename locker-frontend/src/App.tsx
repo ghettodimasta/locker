9078e092b1e550e4848f1c24bfb3c912c8eef66a
ghettodimasta <dima_pavlov1702@mail.ru>
2023-04-04 19:52:28 +0300

    начинаем фронт

---
+++ locker-frontend/src/App.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import {
+  Await,
+  createBrowserRouter,
+  createRoutesFromElements,
+  defer,
+  Form,
+  Link,
+  Outlet,
+  Route,
+  RouterProvider,
+  Routes,
+  useAsyncError,
+  useAsyncValue,
+  useFetcher,
+  useFetchers,
+  useLoaderData,
+  useNavigation,
+  useParams,
+  useRevalidator,
+  useRouteError,
+} from "react-router-dom";
+import Popper from "popper.js";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import './App.css';
+// import SideBar from "./components/AdminPanel/SideBar/SideBar";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+import HomePage from "./components/HomePage/HomePage";
+
+
+let router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<HomePage/>}/>
+      <Route path="*" element={<PageNotFound/>}/>
+    </Route>
+  )
+  // <Route path="/" element={<SideBar/>}>
+  //     {/*<Route index loader={homeLoader} element={<Home />} />*/}
+  //     <Route index element={<AdminPanel/>}/>
+  //     {/*<Route path="about" element={<About />} />*/}
+  //     {/*<Route path="dashboard" element={<Dashboard />} />*/}
+  //     {/*<Route path="*" element={<NoMatch />} />*/}
+  //   </Route>
+);
+
+
+function App() {
+  return <RouterProvider router={router}/>;
+}
+
+// function App() {
+//   return (
+//     <Routes>
+//       <Route path="/" element={<AdminPanel/>}></Route>
+//       {/*<Route path="*" element={<NoMatch />} />*/}
+//     </Routes>
+//   );
+// }
+
+export default App;
